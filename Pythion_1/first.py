3a920f41a3c3a2a8def86f6d3c5f1736f894c883
mansorsalahuddin <mansorsalahuddin9@gmail.com>
2024-10-15 18:07:22 -0400

    i added a new button to github

---
+++ Pythion_1/first.py
@@ -0,0 +1,17 @@
+age = 12
+name = "salahuddin"
+#print(age)
+
+Statement  = '  '
+
+answer = input('is earth flat? enter True or false:    ')
+if answer == 'True':
+    print('you are wrong')
+else:
+    print('your are right')
+
+
+my_variable = 'Hello, world'
+print(my_variable)
+
+
