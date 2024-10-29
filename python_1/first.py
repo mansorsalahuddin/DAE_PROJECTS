e1a216c9d6e0fa35b8bc5771ac2144122644ef37
mansorsalahuddin <mansorsalahuddin9@gmail.com>
2024-10-29 18:04:55 -0400

    pushing it

--- python_1/first.py
+++ python_1/first.py
@@ -1,9 +1,10 @@
 age = 12
 name = "salahuddin"
-#print(age)
+height= 6.0
+print(age)
 
 Statement  = '  '
-
+# True or false 
 answer = input('is earth flat? enter True or false:    ')
 if answer == 'True':
     print('you are wrong')
@@ -19,6 +20,19 @@ print(my_variable)
 #Loop through numbers from 1 to 5
 for i in range(1, 6):
     print(i)
+
+
+
+
+
+colors = ['red','yellow','blue']
+
+
+print(colors[2])
+
+for color in colors:
+    print(color)
+
 
 
 
